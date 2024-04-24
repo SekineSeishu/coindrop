110de87c3088a9a67e513b253725d49a356fd059
SekineSeishu <mika242401@gmail.com>
2024-04-24 12:15:02 +0900

    ゲームマネージャー(コインの初期生成、所持コイン消費して生成)

---
+++ Assets/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private int nowcoin;
+    [SerializeField]
+    private int initial_coin;
+    [SerializeField]
+    private GameObject coinPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < initial_coin; i++)
+        {
+            StartInstantiatecoin();
+        }
+    }
+
+    public void StartInstantiatecoin()
+    {
+        float x = Random.Range(-2, 2);
+        float z = Random.Range(-1, -5);
+        Instantiate(coinPrefab, new Vector3(x, 5, z), coinPrefab.transform.rotation);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (nowcoin != 0)
+            {
+                StartInstantiatecoin();
+                nowcoin--;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        nowcoin++;
+    }
+}
